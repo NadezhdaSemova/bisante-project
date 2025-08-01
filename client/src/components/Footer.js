a4bbb9fc38e993ec861dc98b6d006e2b17488bbe
NadezhdaSemova <106181080+NadezhdaSemova@users.noreply.github.com>
2025-08-01 12:00:58 +0300

    Fix Style

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,14 +1,31 @@
 
 import {Link} from 'react-router-dom'
+import { useEffect, useState } from 'react';
 import '../styles/footer.css';
 
 function Footer() {
+const [showAdminLink, setShowAdminLink] = useState(false);
+
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      if (e.ctrlKey && e.key.toLowerCase() === 'a') {
+        setShowAdminLink(true);
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyPress);
+    return () => window.removeEventListener('keydown', handleKeyPress);
+  }, []);
+
   return (
     <footer>
       <p>© 2025 Винарна Бисанте. Всички права запазени.</p>
 
-      {/* Почти невидим бутон */}
-      <Link to="/admin-login" className="secret-admin-link">Admin</Link>
+      {showAdminLink && (
+        <Link to="/admin-login" className="admin-link-visible">
+          Admin
+        </Link>
+      )}
     </footer>
   );
 }
